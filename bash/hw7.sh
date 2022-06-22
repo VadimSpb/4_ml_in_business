433345c782a0f46ee9f6b7e96faa7e14233742cf
Vadim Mazeiko <Vadim_Mazeiko@epam.com>
2022-06-22 13:57:27 +0300

    hw7 complited

---
+++ bash/hw7.sh
@@ -0,0 +1,4 @@
+wget 'https://drive.google.com/uc?export=download&id=1yIIxDfW7Wfq-wPlbsa0dFrSlD3r-Ai91' -O ./data/churn_data.csv
+conda install catboost
+conda install shap
+jupyter nbextension enable --py widgetsnbextension
